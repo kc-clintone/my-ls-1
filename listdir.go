044ff11df0f274f7c06d82103a943e293d681d0d
faimali <faisalmasiga3054@gmail.com>
2026-03-31 13:22:38 +0300

    update: update the listdir to return only list for flag handling

--- listdir.go
+++ listdir.go
@@ -1,14 +1,67 @@
 package main
 
 import (
-	"io/fs"
+	"fmt"
 	"os"
+	"os/user"
+	"path/filepath"
+	"syscall"
+	"time"
 )
 
-func ListDirectory(path string) ([]fs.DirEntry, error) {
-	entries, err := os.ReadDir(path)
+type FileEntry struct {
+	Name      string
+	IsDir     bool
+	Mode      os.FileMode
+	Size      int64
+	ModTime   time.Time
+	Links     uint64
+	Owner     string
+	Group     string
+	SymlinkTo string
+}
+
+func ListDirectory(path string, flags map[rune]bool) ([]FileEntry, error) {
+	dirEntries, err := os.ReadDir(path)
 	if err != nil {
 		return nil, err
 	}
-	return entries, nil
+
+	result := make([]FileEntry, 0, len(dirEntries))
+
+	for _, entry := range dirEntries {
+		info, err := entry.Info()
+		if err != nil {
+			continue // skip files we can’t stat
+		}
+
+		stat := info.Sys().(*syscall.Stat_t)
+
+		ownerObj, _ := user.LookupId(fmt.Sprint(stat.Uid))
+		groupObj, _ := user.LookupGroupId(fmt.Sprint(stat.Gid))
+
+		var symlinkTo string
+		if info.Mode()&os.ModeSymlink != 0 {
+			target, err := os.Readlink(filepath.Join(path, entry.Name()))
+			if err == nil {
+				symlinkTo = target
+			}
+		}
+
+		fileEntry := FileEntry{
+			Name:      entry.Name(),
+			IsDir:     entry.IsDir(),
+			Mode:      info.Mode(),
+			Size:      info.Size(),
+			ModTime:   info.ModTime(),
+			Links:     stat.Nlink,
+			Owner:     ownerObj.Username,
+			Group:     groupObj.Name,
+			SymlinkTo: symlinkTo,
+		}
+
+		result = append(result, fileEntry)
+	}
+
+	return result, nil
 }
